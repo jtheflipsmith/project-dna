7ca3a97710f5ba6060dbf41dc5746d5d28ddd72a
jtheflipsmith <joshuasmith@byui.edu>
2024-12-09 21:48:42 -0700

    Added HTML, JS, and CSS for main page.

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+// List of objets to be used for navigation folders on the main page
+
+const fPhotos = [
+    {
+        title: "Story",
+        image: ""
+    },
+    {
+        title: "Characters",
+        image: ""
+    },
+    {
+        title: "Settings",
+        image: ""
+    },
+    {
+        title: "Races",
+        image: ""
+    },
+    {
+        title: "Technology",
+        image: ""
+    },
+    {
+        title: "Concept",
+        image: ""
+    }
+]
+
+//foreach loop to iterate through and add each item to the main page.
+
+fPhotos.forEach(fphoto => { 
+    
+});
